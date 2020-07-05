ac7f88e278d270da3a01b26e41aaef4593d82a50
mamenghua <1450125019@qq.com>
2020-07-05 22:31:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/* 滚动条样式更改 */
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+}
+::-webkit-scrollbar-track {
+  background: #fff;
+  border-radius: 10px;
+}
+::-webkit-scrollbar-thumb {
+  background: #c1c1c1;
+  border: 2px solid #fff;
+  border-radius: 10px;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #a8a8a8;
+}
+::-webkit-scrollbar-corner {
+  background: #fff;
+}
+/* 兼容IE浏览器 */
+body {
+  scrollbar-arrow-color: #cdcdcd; /*三角箭头的颜色*/
+  scrollbar-face-color: #cdcdcd; /*立体滚动条的颜色*/
+  scrollbar-3dlight-color: #fff; /*立体滚动条亮边的颜色*/
+  scrollbar-highlight-color: #fff; /*滚动条空白部分的颜色*/
+  scrollbar-shadow-color: #fff; /*立体滚动条阴影的颜色*/
+  scrollbar-darkshadow-color: #fff; /*立体滚动条强阴影的颜色*/
+  scrollbar-track-color: #fff; /*立体滚动条背景颜色*/
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="scrollDiv">
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ex
+          quia ab aliquid facere architecto culpa dolorum fugit corrupti harum
+          accusantium quisquam inventore et magnam itaque minima hic! Impedit
+          error.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          incidunt quia ipsam totam tenetur maxime repellendus possimus debitis
+          molestiae velit vel fugiat ullam excepturi est quisquam quo fugit
+          culpa amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
+          facilis ipsam iste repellat sint numquam explicabo voluptatum
+          voluptate corporis fugit adipisci qui exercitationem corrupti debitis
+          tempore omnis maxime at quidem!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          voluptatem dolores ab dolorem est itaque ut sit deserunt qui
+          laudantium eaque vitae perspiciatis amet quasi unde inventore dolor
+          quis. Natus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quia
+          doloremque blanditiis quas unde nostrum a voluptatibus quam
+          perspiciatis non quaerat enim neque quos rerum quo dolor expedita
+          error deserunt!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      msg: "Test页面，滚动条"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.scrollDiv {
+  width: 200px;
+  height: 200px;
+  overflow: auto;
+  border: 10px solid red;
+}
+.scrollDiv div {
+  width: 400px;
+}
+</style>
