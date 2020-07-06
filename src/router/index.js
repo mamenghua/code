6511cd98fa8d747123a00cd83a579aaec3f4e5f9
mamenghua <1450125019@qq.com>
2020-07-06 22:16:55 +0800

    echarts绘制矩形图标

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Test from '@/views/Test'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Test from "@/views/Test";
+import Echarts from "@/views/Echarts";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
     },
     {
-      path: '/test',
-      name: 'Test',
+      path: "/test",
+      name: "Test",
       component: Test
+    },
+    {
+      path: "/echarts",
+      name: "Echarts",
+      component: Echarts
     }
   ]
-})
+});
---
+++ src/views/echarts.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    {{ msg }}
+    <div id="echartsDiv"></div>
+  </div>
+</template>
+
+<script>
+import echarts from "echarts";
+export default {
+  name: "Echarts",
+  data() {
+    return {
+      msg: "Echarts页面"
+    };
+  },
+  mounted() {
+    let myChart = echarts.init(document.getElementById("echartsDiv"));
+    let option = {
+      title: {
+        text: "ECharts 入门示例"
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"]
+      },
+      xAxis: {},
+      yAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+          backgroundStyle: {
+            color: "rgba(220, 220, 220, 0.8)"
+          }
+        }
+      ]
+    };
+    myChart.setOption(option);
+  }
+};
+</script>
+
+<style scoped>
+#echartsDiv {
+  width: 600px;
+  height: 300px;
+}
+</style>
